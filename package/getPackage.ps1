df7edae0c17004b29a73ee747847e6b17be0b6f6
Nuevo009 <kianakaslanna6@gmail.com>
2021-12-01 22:01:22 +0800

    update

--- package/getPackage.ps1
+++ package/getPackage.ps1
@@ -12,24 +12,11 @@ foreach ($i in $latest.voice_packs) {
     Write-Output "`n" >> .\package.md
 }
 
-$pre_dowload = $a.data.pre_download_game.latest
-Write-Output "`n# pre_download`n" >> .\package.md
-Write-Output "## game`n" >> .\package.md
-Write-Output "`n### path`n $($pre_dowload.path)" >> .\package.md
-Write-Output "`n### md5`n $($pre_dowload.md5)" >> .\package.md
-Write-Output "`n## voice_packs`n" >> .\package.md
-foreach ($i in $pre_dowload.voice_packs) {
-    Write-Output "`n***`n### $($i.language)" >> .\package.md
-    Write-Output "`n#### path`n$($i.path)" >> .\package.md
-    Write-Output "`n#### md5 `n$($i.md5)" >> .\package.md
-    Write-Output "`n" >> .\package.md
-}
-
-$diffs = $a.data.pre_download_game.diffs
+$diffs = $a.data.game.diffs[0]
 Write-Output "`n# diff`n`n" >> .\package.md
 Write-Output "## game`n`n" >> .\package.md
-Write-Output `npath: $diffs[0].path >> .\package.md
-Write-Output `nmd5: $diffs[0].md5 >> .\package.md
+Write-Output "`n### path`n $($a.path)"  >> .\package.md
+Write-Output "`n### md5`n $($a.md5)" >> .\package.md
 Write-Output "`n## voice_packs`n`n" >> .\package.md
 foreach ($i in $diffs.voice_packs) {
     Write-Output "`n***`n### $($i.language)" >> .\package.md
